55344528fae74a7504874bd4b51c9ab71d171505
Lautaro <selaufe@gmail.com>
2021-12-22 19:28:23 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,7 +10,7 @@ const nuevoNum = 4;
 const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 3 === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
@@ -35,7 +35,7 @@ function suma(x, y) {
   // "x" e "y" son tectnúmeros
   // Suma "x" e "y" juntosnpm test JSI.test.js y devuelve el valor
   // Tu código:
-suma(2, 3)
+  suma(3, 4);
 }
 
 function resta(x, y) {

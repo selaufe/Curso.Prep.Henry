21163e4eb99c262720330b6c87f42759849c525f
Lautaro <selaufe@gmail.com>
2022-03-03 20:05:59 -0300

    Probando error

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -25,11 +25,6 @@ function obtenerLargoDelArray(array) {
 
 
 function incrementarPorUno(array) {
-  var nuevoArray = [];
-  for(var i = 0; i < array.length; i++) {
-    nuevoArray[i] = array[i] + 1;
-  }
-  return nuevoArray;
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
